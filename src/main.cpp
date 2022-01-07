e3bcbf313d12f73d7c739e955ba31fcc165d52ef
milenacsilva <milenacorreasilva@usp.br>
2022-01-07 17:11:16 -0300

    commenting main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,14 @@
 
 using namespace std;
 
+/**
+ * @brief Draws and checks for interactions with the UI Elements.
+ * 
+ * @param el Element to be handled.
+ * @param mouseInput Mouse input 
+ * @return true If element still has value.
+ * @return false If the element has been cleared.
+ */
 bool handleUI(shared_ptr<UIElement> el, raylib::Mouse& mouseInput) {
     if (el == nullptr) return true;
     el->draw();
@@ -31,6 +39,18 @@ bool handleUI(shared_ptr<UIElement> el, raylib::Mouse& mouseInput) {
     return r;
 }
 
+/**
+ * @brief Loops throught all the Scenes of the 
+ * game, drawing their components once they put it
+ * on the drawStack or drawStatic
+ * 
+ * @param win Game window.
+ * @param drawStack Vector that draws the stack elements. 
+ * @param drawStatic Vector that draws the stack elements.
+ * @param curScene Pointer to the current scene.
+ * @return true To remain in the gameLoop.
+ * @return false To leave the game.
+ */
 bool gameLoop(
     shared_ptr<raylib::Window>& win,
     shared_ptr<Scene>& currentScene,
